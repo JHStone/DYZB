18c5b2728b642d6cdf4f29d255d9f0c212e6e2a5
谷建华 <gujianhua@gujianhuadeMacBook-Pro-2.local>
2017-03-15 17:10:39 +0800

    阿斯蒂芬

--- DYZB/DYZB/Classes/Home/Controller/RecommendViewController.swift
+++ DYZB/DYZB/Classes/Home/Controller/RecommendViewController.swift
@@ -8,12 +8,32 @@
 
 import UIKit
 
+fileprivate var normalItemID : String = "normalItemID"
+
+fileprivate var itemSpace : CGFloat = 10
+
+
 class RecommendViewController: UIViewController {
+    
+    fileprivate lazy var collectionView : UICollectionView = {[weak self] in
+        let layout = UICollectionViewFlowLayout()
+        let itemW = (screenW - 3 * itemSpace) / 2
+        let itemH = itemW / 4 * 3
+        layout.itemSize = CGSize(width: itemW, height: itemH)
+        layout.minimumLineSpacing = 0
+        layout.minimumInteritemSpacing = itemSpace
+        let collection = UICollectionView(frame: (self?.view.bounds)!, collectionViewLayout: layout)
+        collection.dataSource = self
+        collection.backgroundColor = UIColor.gray
+        collection.register(UICollectionViewCell.self, forCellWithReuseIdentifier: normalItemID)
+        collection.contentInset = UIEdgeInsetsMake(0, 10, 0, 10)
+        return collection
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.red
-        // Do any additional setup after loading the view.
+        setupUI()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +41,34 @@ class RecommendViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+}
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+extension RecommendViewController{
+    func setupUI(){
+        view.addSubview(collectionView)
+       
     }
-    */
+}
 
+extension RecommendViewController : UICollectionViewDataSource {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 12
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if section == 0 {
+            return 8
+        }
+        return 4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let item = collectionView.dequeueReusableCell(withReuseIdentifier: normalItemID, for: indexPath)
+        item.backgroundColor = UIColor.red
+        return item
+    }
+    
+    
 }
